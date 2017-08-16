aa45d4fde07ff530d02c7d9f1d15e5835449c56e
bfchengnuo <bfchengnuo@gmail.com>
2017-08-16 19:54:33 +0800

    add crud and jpql

--- XC/JPA/src/test/java/com/bfchengnuo/domain/PersonTest.java
+++ XC/JPA/src/test/java/com/bfchengnuo/domain/PersonTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 /**
  * Person Tester.
@@ -35,6 +36,70 @@ public class PersonTest {
         factory.close();
     }
 
+    @Test
+    public void select() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpaDemo");
+        EntityManager em = factory.createEntityManager();
 
+        // 相当于 hibernate 中的 get
+        // Person person = em.find(Person.class, 1);
+        // 相当于 hibernate 中的 load ，延迟加载
+        Person person = em.getReference(Person.class, 1);
+        System.out.println(person.getName());
+
+        em.close();
+        factory.close();
+    }
+
+    @Test
+    public void update() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpaDemo");
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+
+        Person person = em.find(Person.class, 1);
+        // 托管状态下提交事务时修改会自动更新到数据库
+        person.setName("沛凝");
+
+        em.getTransaction().commit();
+        em.close();
+        factory.close();
+    }
+
+    @Test
+    public void del() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpaDemo");
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+
+        Person person = em.find(Person.class, 3);
+        em.remove(person);
+
+        em.getTransaction().commit();
+        em.close();
+        factory.close();
+    }
+
+    @Test
+    public void jpqlTest() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpaDemo");
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
+
+        // 推荐使用指定结果集，这样就不用强转了
+        TypedQuery<Person> query =
+                em.createQuery("SELECT e FROM Person e where e.id=?",
+                        Person.class); // 指定索引从0开始： ?0
+        query.setParameter(0,1);
+        // 获取单个数据，第一条
+        Person singleResult = query.getSingleResult();
+        System.out.println(singleResult.getName());
+        // 获取全部的数据集
+        // List<Person> list = query.getResultList();
+
+        em.getTransaction().commit();
+        em.close();
+        factory.close();
+    }
 
 } 
